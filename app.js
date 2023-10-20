8ad7cf9804bf45d9c766128f3ad3b9b7eeda4b91
OCassador <148351070+OCassador@users.noreply.github.com>
2023-10-20 09:08:20 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+let listaNumero = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagenmTentativas = `Voce descobriu o numero secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagenmTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled')
+
+
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O numero secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O numero secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido =  parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeElemento = listaNumero.length;
+
+    if (quantidadeElemento == numeroLimite) {
+        listaNumero = [];
+
+    }
+    if (listaNumero.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaNumero.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disbled', true);
+
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do numero secreto');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 a 10');
+}
